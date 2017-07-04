73cf207dcaff9c77a6378e09bc0dbb30c3fc18f6
snoops <snoops@list.ru>
2017-07-04 13:38:42 +0400

    add methods for trains

--- app/models/train.rb
+++ app/models/train.rb
@@ -7,4 +7,16 @@ class Train < ApplicationRecord
 
   validates :name, presence: true
 
+  def wagon_type(type_w)
+     self.wagons.where(type_class: type_w)
+     self.wagons.where(type_class: type_w).count
+  end
+
+  def wagon_down_seats(type_w, place_down)
+    self.wagons.where(type_class: type_w).sum(place_down)
+  end
+
+  def wagon_up_seats(type_w, place_up)
+    self.wagons.where(type_class: type_w).sum(place_up)
+  end
 end
