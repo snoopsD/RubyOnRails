d42259d0f0caea14d0cb5007e5b72823a73788e2
snoops <snoops@list.ru>
2017-08-02 16:52:51 +0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,7 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
-  get 'welcome/index'
-
-  root 'welcome#index'
+  root 'searches#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
