b8afc595487a59cc41902a3751d37bd00cfcd688
snoops <snoops@list.ru>
2017-08-04 10:56:46 +0400

    Update some

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+  root 'searches#new'
+  
   devise_for :users
 
   resource :search, only: [:new, :show, :create]
   resources :tickets, except: [:edit, :update]
 
   namespace :admin do
+
+    get "index", to: 'base#index'
+
     resources :railway_stations do
       patch :update_position, on: :member
       patch :update_arrival, on: :member
       patch :update_departure, on: :member
+
+
     end
 
     resources :trains do
@@ -19,7 +26,9 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
-  root 'searches#new'
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
